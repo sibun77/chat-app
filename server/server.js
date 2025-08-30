601dfc92e0f8e5f6cd257575711f799a56472453
Shibabrata Jena <shibabrataj@gmail.com>
2025-08-30 18:57:37 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -47,7 +47,10 @@ app.use("/api/messages",messageRouter)
 // Connect to the DB
 connectDB()
 
-
-const PORT = process.env.PORT || 5000
+if(process.env.NODE_ENV !== 'production'){
+    const PORT = process.env.PORT || 5000
 server.listen(PORT,()=> console.log("Server is running on PORT: "+PORT))
+}
 
+//export server for vercel
+export default server;
