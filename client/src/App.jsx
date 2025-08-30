d86d26238477185e04140da675bafc9f63b5f671
Shibabrata Jena <shibabrataj@gmail.com>
2025-08-30 19:03:36 +0530

    image issue fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const {authUser} = useContext(AuthContext)
   
   return (
-    <div className='bg-[url("./src/assets/bgImage.svg")] bg-cover bg-no-repeat'>
+    <div className='bg-[url("/bgImage.svg")] bg-cover bg-no-repeat'>
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login" />}/>
